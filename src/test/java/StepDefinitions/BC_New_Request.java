c71fdb777ee170b6a77880154c4599b6fdaf4855
N.S <453123@cognizant.com>
2021-01-18 17:39:44 +0530

    commit from Geetha develop

--- src/test/java/StepDefinitions/BC_New_Request.java
+++ src/test/java/StepDefinitions/BC_New_Request.java
@@ -38,7 +38,7 @@ public class BC_New_Request extends Base {
 		login.getUsername().sendKeys(strArg1);
 		login.getPassword().sendKeys(strArg2);
 		login.getBtnLogin().click();
-		System.out.println("hi this is the final msg");
+		System.out.println("hi this is the finallll msg");
     }
 
     @When("^the New Request button is clicked$")
